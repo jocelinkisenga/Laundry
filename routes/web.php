7bf63e95085d00884141f69df7da1a7b52ec68bb
jocelin kisenga <kisengajocelin7@gmail.com>
2024-01-14 10:03:08 +0200

    FEAT added routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware("admin")->group(function () {
+
+    Route::get("/users",[UserController::class,"index"])->name("admin.users");
+    Route::get("/user",[UserController::class,"create"])->name("admin.user.create");
+    Route::post("/user",[UserController::class,"store"])->name("admin.user.store");
+    Route::put("/userUpdate",  [UserController::class, "update"])->name("admin.user.update");
+    Route::get("/user/{id}",[UserController::class,"show"])->name("admin.user.show");
+
+    Route::get("/roles",[RoleController::class,"index"])->name("admin.roles");
+    Route::get("/role",[RoleController::class,"create"])->name("admin.role.create");
+    Route::post("/role",[RoleController::class,"store"])->name("admin.role.store");
+    Route::put("/roleUpdate",  [RoleController::class, "update"])->name("admin.role.update");
+    Route::get("/role/{id}",[RoleController::class,"show"])->name("admin.role.show");
+
+});
+
 
 Route::middleware([
     'auth:sanctum',
